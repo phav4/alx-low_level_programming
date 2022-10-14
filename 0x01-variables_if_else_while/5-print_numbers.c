7680dfc8bd8d588875952d1f9dec13cfe45c85ae
Favour <divinephavor@gmail.com>
2022-10-14 06:58:57 -0700

    Task 5 correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-	int n;
-	while (n < 10)
-	{
-		printf("%d", n);
-		n += 1;
-	}
-	printf("\n");
-	return (0);
+int n;
+while (n < 10)
+{
+printf("%d", n);
+n += 1;
+}
+printf("\n");
+return (0);
 }
