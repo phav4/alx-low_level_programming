52447d5331e4344fd3febb156db4aa0bab307e51
Favour <divinephavor@gmail.com>
2022-11-17 11:20:16 -0800

    fix code for header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,17 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct type - Struct data type
+ *
+ * @op: data type argument
+ * @f: The function associated
+ */
+
+typedef struct type
+{
+	char *op;
+	void (*f)(va_list, char *);
+}
+
 #endif
